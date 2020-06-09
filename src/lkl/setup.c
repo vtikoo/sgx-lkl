0604b4db0d759f97b87af48143c988a458539791
Vikas Tikoo <vtikoo@users.noreply.github.com>
2020-06-09 05:07:27 +0000

    include lthread header file

--- src/lkl/setup.c
+++ src/lkl/setup.c
@@ -39,6 +39,8 @@
 #include "libdevmapper.h"
 
 #include "enclave/enclave_util.h"
+#include "enclave/lthread.h"
+#include "enclave/sgxlkl_config.h"
 #include "enclave/sgxlkl_t.h"
 #include "enclave/wireguard.h"
 #include "enclave/wireguard_util.h"
